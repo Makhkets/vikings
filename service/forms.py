ac7a092526ba7215c3bb6776241c0cc87b82266f
ENCODEE <ahdan042@gmail.com>
2024-07-06 17:23:44 +0300

    массовое добавление фотографий

--- service/forms.py
+++ service/forms.py
@@ -1,4 +1,13 @@
 from django import forms
 
-class PhotoForm(forms.Form):
-    photo = forms.ImageField()
+class MultiplePhotoForm(forms.Form):
+    images = forms.ImageField(
+        widget=forms.ClearableFileInput(attrs={'multiple': True, 'class': 'custom-file-input'}),
+        label="Выбрать изображения",
+        required=False,
+        label_suffix=''
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(MultiplePhotoForm, self).__init__(*args, **kwargs)
+        self.fields['images'].label_class = "custom-label-class"
